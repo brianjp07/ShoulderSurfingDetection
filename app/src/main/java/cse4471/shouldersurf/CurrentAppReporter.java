25ffd9b7ad7577c70f174053dabe7cdd96f0f6f1
Brian <brian.parks07@gmail.com>
2015-04-11 10:42:36 -0400

    trying to get a background service to work that will check what app is open every minute.

---
+++ app/src/main/java/cse4471/shouldersurf/CurrentAppReporter.java
@@ -0,0 +1,74 @@
+package cse4471.shouldersurf;
+
+import android.app.ActivityManager;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Brian on 4/10/2015.
+ * This is the background service.
+ */
+
+public class CurrentAppReporter extends IntentService {
+    private static final String serviceName = CurrentAppReporter.class.getSimpleName();
+    private Timer timer;
+    private TimerTask updateTask = new TimerTask(){
+        @Override
+        public void run(){
+            Log.i(serviceName,"Doin work");
+        }
+    };
+
+    public CurrentAppReporter(){
+        super("CurrentAppReporter");
+    }
+    @Override
+    protected void onHandleIntent(Intent workIntent) {
+        // Gets data from the incoming Intent
+        String dataString = workIntent.getDataString();
+
+        // Do work here, based on the contents of dataString
+
+    }
+    //used for others services to connect to this one but since we don't need that return null.
+    @Override
+    public IBinder onBind(Intent intent){return null;}
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.i(serviceName,"Service Created");
+        timer = new Timer("AppCheckerTimer");
+        Context context = this;
+        String currentApp = null;
+        ActivityManager activityManager = (ActivityManager) context.getSystemService( Context.ACTIVITY_SERVICE );
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+        for(ActivityManager.RunningAppProcessInfo ap : appProcesses){
+            if(ap.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                currentApp = ap.processName;
+                Log.i("Foreground App", ap.processName);
+            }
+        }
+        //schedule time to repeat every 60 seconds
+        timer.schedule(updateTask,1000L,60*1000L);
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i(serviceName,"Service Destroyed");
+        timer.cancel();
+        timer = null;
+    }
+
+
+}
+
--- app/src/main/java/cse4471/shouldersurf/MainActivity.java
+++ app/src/main/java/cse4471/shouldersurf/MainActivity.java
@@ -1,13 +1,15 @@
 package cse4471.shouldersurf;
 
 import android.app.ActivityManager;
+import android.app.IntentService;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import java.util.Iterator;
 import java.util.List;
 
@@ -18,18 +20,31 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //look  at the logs to see result.
+        //getCurrentApp();
+
         Context context = this;
+        Intent serviceIntent = new Intent(context,CurrentAppReporter.class);
+        //serviceIntent.setAction("cse4471.shouldersurf.CurrentAppReporter");
+        startService(serviceIntent);
+
+
+    }
+
+    //this will be called by the background service to get the current app.
+    public String getCurrentApp(){
+        Context context = this;
+        String currentApp = null;
         ActivityManager activityManager = (ActivityManager) context.getSystemService( Context.ACTIVITY_SERVICE );
         List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
         for(ActivityManager.RunningAppProcessInfo ap : appProcesses){
             if(ap.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                currentApp = ap.processName;
                 Log.i("Foreground App", ap.processName);
             }
         }
+        return currentApp;
     }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
